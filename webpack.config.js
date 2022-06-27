e1b5315b50750177a7359d10000ae33ebff2db82
stacksparrow4 <stacksparrow4@gmail.com>
2022-06-27 17:18:35 +1000

    Added source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
     entry: "./src/main.js",
     mode: "development",
+    devtool: "inline-source-map",
     output: {
         filename: "bot.js",
         path: path.resolve(__dirname, "dist"),
