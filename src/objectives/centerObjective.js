8c644cd9307ddcf36529a15963e01d2000bdf3d5
a-jason-liu21 <a.jason.liu21@gmail.com>
2022-06-28 12:49:38 +1000

    objective framework

---
+++ src/objectives/centerObjective.js
@@ -0,0 +1,21 @@
+import canvasUtil from "../canvasUtil.js";
+// template goal which makes the snake tend towards the center
+
+const centerObjective = {
+  getAction: () => {
+    return { target_x: 20000, target_y: 20000, boost: false };
+  },
+
+  getPriority: () => {
+    return 0;
+  },
+
+  drawDebug: () => {
+    const p1 = canvasUtil.point(0, 0);
+    const p2 = canvasUtil.point(window.snake.xx, window.snake.yy);
+    canvasUtil.drawLine(p1, p2, "#FFFFFF", 5);
+    return;
+  },
+};
+
+export default centerObjective;
