22305ff693d430ea6f02c3b063b54a8c403ecae5
stacksparrow4 <stacksparrow4@gmail.com>
2022-07-02 08:51:18 +1000

    Bug fix and format bot.js

--- src/objectives/centerObjective.js
+++ src/objectives/centerObjective.js
@@ -2,20 +2,20 @@ import canvasUtil from "../canvasUtil.js";
 // template goal which makes the snake tend towards the center
 
 const centerObjective = {
-  getAction: () => {
-    return { target_x: 20000, target_y: 20000, boost: false };
-  },
+    getAction: () => {
+        return { target_x: 20000, target_y: 20000, boost: false };
+    },
 
-  getPriority: () => {
-    return 0;
-  },
+    getPriority: () => {
+        return 0;
+    },
 
-  drawDebug: () => {
-    const p1 = canvasUtil.point(0, 0);
-    const p2 = canvasUtil.point(window.snake.xx, window.snake.yy);
-    canvasUtil.drawLine(p1, p2, "#FFFFFF", 5);
-    return;
-  },
+    drawDebug: () => {
+        const p1 = canvasUtil.point(20000, 20000);
+        const p2 = canvasUtil.point(window.snake.xx, window.snake.yy);
+        canvasUtil.drawLine(p1, p2, "#FFFFFF", 5);
+        return;
+    },
 };
 
 export default centerObjective;
