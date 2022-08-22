a647cf5be2cc0e672d8b0f90ea06848ff8ae10cd
RYNO8 <35283610+RYNO8@users.noreply.github.com>
2022-08-22 22:33:17 +1000

    obstacble avoid inverse distance

--- src/objectives/avoidObjective.js
+++ src/objectives/avoidObjective.js
@@ -1,11 +1,11 @@
 import canvasUtil from "../canvasUtil.js";
 import { getSnakes } from "./util";
+import bot from "../bot";
 
-const HEAD_DISTANCE_FACTOR = 30000;
-const BODY_DISTANCE_FACTOR = 300;
-const BASE_RAD = 15;
+// its bad when youre less than 1.5 radii from the center of a snake
+const CLOSEST_MULT = 2.5;
 
-const ENABLE_THRESHOLD = 0.012;
+const ENABLE_THRESHOLD = 1e-9;
 
 const DEBUG_GAP = 50;
 const DEBUG_RAD = DEBUG_GAP * 10;
@@ -13,71 +13,37 @@ const DEBUG_NORM = 2;
 const DEBUG_SHOW_AHEAD = 20;
 const DEBUG_RAY_LEN = 10;
 
+function normDist(pointA, pointB, radius) {
+    const minDist = CLOSEST_MULT * Math.max(bot.getSnakeWidth(), radius);
+    return (Math.pow(pointA.xx - pointB.xx, 2) + Math.pow(pointA.yy - pointB.yy, 2)) / minDist;
+}
+
 const getPointGradientFunction = () => {
     const snakes = getSnakes();
 
-    return (x, y) => {
-        return snakes
-            .map((s) => {
-                const size_factor = s.radius / BASE_RAD;
+    return (point) => {
+        let val = 0;
+        let dx = 0;
+        let dy = 0;
+        for (const s of snakes) {
+            const dist = normDist(point, s, s.radius);
+            val += 1 / Math.pow(dist, 4);
+            dx += 2 * (s.xx - point.xx) / Math.pow(dist, 5);
+            dy += 2 * (s.yy - point.yy) / Math.pow(dist, 5);
 
-                const distance_from_head =
-                    Math.pow(s.xx - x, 2) + Math.pow(s.yy - y, 2);
-                const head_value = Math.exp(
-                    -distance_from_head / (HEAD_DISTANCE_FACTOR * size_factor)
-                );
-                const partial_x =
-                    head_value * ((-2 * (x - s.xx)) / HEAD_DISTANCE_FACTOR);
-                const partial_y =
-                    head_value * ((-2 * (y - s.yy)) / HEAD_DISTANCE_FACTOR);
+            for (const seg of s.segments) {
+                const dist = normDist(point, seg, s.radius);
+                val += 1 / Math.pow(dist, 4);
+                dx += 2 * (seg.xx - point.xx) / Math.pow(dist, 5);
+                dy += 2 * (seg.yy - point.yy) / Math.pow(dist, 5);
+            }
+        }
 
-                const body_value = s.segments
-                    .map((seg) => {
-                        const distance_from_seg =
-                            Math.pow(seg.xx - x, 2) + Math.pow(seg.yy - y, 2);
-                        const seg_val = Math.exp(
-                            -distance_from_seg /
-                                (BODY_DISTANCE_FACTOR * size_factor)
-                        );
-                        const partial_x =
-                            seg_val *
-                            ((-2 * (x - seg.xx)) / BODY_DISTANCE_FACTOR);
-                        const partial_y =
-                            seg_val *
-                            ((-2 * (y - seg.yy)) / BODY_DISTANCE_FACTOR);
-
-                        return { val: seg_val, dx: partial_x, dy: partial_y };
-                    })
-                    .reduce(
-                        (a, b) => ({
-                            val: a.val + b.val,
-                            dx: a.dx + b.dx,
-                            dy: a.dy + b.dy,
-                        }),
-                        {
-                            val: 0,
-                            dx: 0,
-                            dy: 0,
-                        }
-                    );
-                return {
-                    val: head_value + body_value.val,
-                    dx: partial_x + body_value.dx,
-                    dy: partial_y + body_value.dy,
-                };
-            })
-            .reduce(
-                (a, b) => ({
-                    val: a.val + b.val,
-                    dx: a.dx + b.dx,
-                    dy: a.dy + b.dy,
-                }),
-                {
-                    val: 0,
-                    dx: 0,
-                    dy: 0,
-                }
-            );
+        return {
+            val: val,
+            dx: dx,
+            dy: dy
+        };
     };
 };
 
@@ -91,13 +57,13 @@ const avoidObjective = {
     },
 
     getAction: function () {
-        const dirGrad = this.gradFunc(window.snake.xx, window.snake.yy);
+        const { dx, dy } = this.gradFunc(window.snake);
 
         const len = Math.sqrt(
-            Math.pow(dirGrad.dx, 2) + Math.pow(dirGrad.dy, 2)
+            Math.pow(dx, 2) + Math.pow(dy, 2)
         );
-        const dir_x = -(100 * dirGrad.dx) / len;
-        const dir_y = -(100 * dirGrad.dy) / len;
+        const dir_x = -(100 * dx) / len;
+        const dir_y = -(100 * dy) / len;
 
         return {
             target_x: window.snake.xx + dir_x,
@@ -107,34 +73,35 @@ const avoidObjective = {
     },
 
     getPriority: function () {
-        const { val } = this.gradFunc(window.snake.xx, window.snake.yy);
+        const { val } = this.gradFunc(window.snake);
+        //console.log(val);
         const normalised = val * 2 / ENABLE_THRESHOLD - 1;
         return Math.max(-1, Math.min(1, normalised));
     },
 
     drawDebug: function () {
-        if (window.visualDebugging) {
+        if (window.visualDebugging && false) {
             const startX = window.snake.xx - DEBUG_RAD;
             const endX = window.snake.xx + DEBUG_RAD;
             const startY = window.snake.yy - DEBUG_RAD;
             const endY = window.snake.yy + DEBUG_RAD;
 
             const cachedVals = [];
-            for (let x = startX; x <= endX; x += DEBUG_GAP) {
-                for (let y = startY; y <= endY; y += DEBUG_GAP) {
-                    cachedVals.push(this.gradFunc(x, y).val);
+            for (let xx = startX; xx <= endX; xx += DEBUG_GAP) {
+                for (let yy = startY; yy <= endY; yy += DEBUG_GAP) {
+                    cachedVals.push(this.gradFunc({ xx: xx, yy: yy }).val);
                 }
             }
 
             let i = 0;
-            for (let x = startX; x <= endX; x += DEBUG_GAP) {
-                for (let y = startY; y <= endY; y += DEBUG_GAP) {
+            for (let xx = startX; xx <= endX; xx += DEBUG_GAP) {
+                for (let yy = startY; yy <= endY; yy += DEBUG_GAP) {
                     const col = Math.min(
                         Math.floor((255 * cachedVals[i]) / DEBUG_NORM),
                         255
                     );
                     canvasUtil.drawCircle(
-                        { x, y },
+                        { xx, yy },
                         `rgb(${col}, ${col}, ${col})`,
                         true
                     );
@@ -147,7 +114,7 @@ const avoidObjective = {
         let prev_y = window.snake.yy;
 
         for (let i = 0; i < DEBUG_SHOW_AHEAD; i++) {
-            const dirGrad = this.gradFunc(prev_x, prev_y);
+            const dirGrad = this.gradFunc({ xx: prev_x, yy: prev_y });
 
             if (i === 0 && dirGrad.val < ENABLE_THRESHOLD) {
                 // We are not enabled, dont show visual
