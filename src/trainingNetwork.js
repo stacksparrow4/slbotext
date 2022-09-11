fd3f4263fba135f490bd9343e15db8e6ea585311
stacksparrow4 <stacksparrow4@gmail.com>
2022-09-11 10:00:52 +1000

    tmp

--- src/trainingNetwork.js
+++ src/trainingNetwork.js
@@ -1,3 +1,5 @@
+import { loadGraphModel } from "@tensorflow/tfjs-converter";
+
 let trainingWS;
 let hasStartedRound = false;
 
@@ -10,7 +12,7 @@ export function trainingRoundTerminated() {
     if (!hasStartedRound) {
         return;
     }
-    console.log("unbliucky");
+    console.log("unlucky");
     hasStartedRound = false;
 }
 
@@ -25,10 +27,30 @@ export function connectToTrainingServer() {
         if (event.data === "pong") {
             console.log("Connected to training server");
 
+            trainingWS.send("reqnet");
+
             document
                 .getElementById("playh")
                 .firstChild.firstChild.querySelector(".nsi")
                 .click();
+        } else if (event.data.startsWith("resnet=")) {
+            const netData = event.data.substring(7);
+
+            const parts = netData.split("|");
+
+            parts.forEach((part) => {
+                const [fname, fvalb64] = part.split(":");
+                const fval = Buffer.from(fvalb64, "base64");
+                localStorage.setItem(fname);
+            });
+
+            // localStorage.setItem("curr_model", jsonData);
+
+            // const model = loadGraphModel("localstorage://curr_model");
+
+            // console.log("Loaded model");
+
+            // console.log(model);
         }
     });
 }
